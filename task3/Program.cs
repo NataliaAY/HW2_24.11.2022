dc11492503c11a00b3833271a9294f5204565a55
Natalia <averegina@male.ru>
2022-11-27 17:54:13 +0300

    решена задача 3

---
+++ task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("Введите день недели...");
+int dayofweek = int.Parse (Console.ReadLine());
+if (dayofweek == 6 || dayofweek == 7) Console.WriteLine ("Выходной");
+else if (dayofweek <= 5) Console.WriteLine ("Будний");
+else if (dayofweek > 7) Console.WriteLine ("Это не день недели");
